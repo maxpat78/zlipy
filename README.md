82c57a57368e8cde2dd0854122793494acfd22d5
maxpat78 <maxpat78@yahoo.it>
2024-12-29 09:17:48 +0100

    Fixed analyze_matches
    Added missed import in analyze_matches

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
-# zlipy
+# zlipy (beta)
 
 A Python 3 package to explore Deflate compression (RFC1951) and analyze its compressed bit streams with a zlib fashion.
 
 Inspired by [pyflate](https://github.com/pfalcon/pyflate) and [acefile](https://github.com/droe/acefile) - if decompression is possible, why not?
 
+With the help of ChatGPT and 1-million-or-so kicks in its virtual ass to get (almost) proper code.
+
 It implements:
-- compression and decompression with Static and Dynamic Huffman trees (or no compression);
+- one-step compression and decompression with Static and Dynamic Huffman trees (or no compression);
 - some HC3 LZ77, Deflate-style match finders in pure Python (use PyPy to boost performances) and one in a C++17 module to get reasonable speed;
 - Huffman encoder and decoder;
 - a bit stream reader and writer;
 - minimal ZIP reader and writer classes;
 - adler32 and crc32.
+
+It does _not_ implement (yet):
+- progressive stream (de)compression (a.k.a. _xxxpress()_ and _flush()_);
+- optimal matches selection/better statistics;
+- compression mixing static/dynamic Huffman and uncompressed blocks;
+- other match finders (i.e. BT);
+- speed and efficiency.
--- sample/analyze_matches.py
+++ sample/analyze_matches.py
@@ -1,8 +1,74 @@
 # -*- coding: windows-1252 -*-
-import sys
-from lz_tester import stats, check
+import sys, sqlite3, datetime
 from zlipy.minizip import MiniZipReader, MiniZipWriter
 from zlipy.infldbg import decompress
+
+#
+# DEBUG tools
+#
+def expand(matches):
+    "Espande una lista di byte e match nel buffer originale"
+    out = bytearray()
+    for o in matches:
+        if isinstance(o, tuple):
+            length, distance = o
+            if length < 3 or length > 258:
+                print(f'lz_expand: match length {length} is out of Deflate limits 3..258')
+                return None
+            if distance < 1 or distance > 32768:
+                print(f'lz_expand: match distance {distance} is out of Deflate limits 1..32768')
+                return None
+            if length >= distance:
+                s = length * out[-distance:] # ripete la sequenza (finale)
+                out.extend(s[:length]) # la limita a length
+            else:
+                out.extend(out[-distance:-distance+length]) # copia i byte esistenti
+        else:
+            out.extend(o.to_bytes(1))
+    return out
+
+global_errors_count = 0
+
+def check(matches, s):
+    "Verifica che l'elenco di match si espanda nella stringa s"
+    s1 = expand(matches)
+    if s != s1:
+        print('check: LZ matches build a different string!')
+        global_errors_count += 1
+        #~ print(s1)
+        return 0
+    return 1
+
+def stats(matches):
+    n = 0
+    offsets = 0
+    lengths = 0
+    # colleziona i dati
+    for m in matches:
+        if isinstance(m, tuple):
+            n += 1
+            lengths += m[0]
+            offsets += m[1]
+    D = {'count': n, 'compressed': lengths, 'stored': len(matches)-n, 'mean_distance': offsets/(n or 1)}
+    print(f"{n} matches for {lengths}/{lengths+D['stored']} bytes. Mean distance {D['mean_distance']}.")
+    return D
+
+find_matches = find_matches_hc3
+#~ find_matches = find_longest_matches
+
+""" stringhe di test
+Input = b'abc' + 300*b'x' + b'abcdabcabc'
+Ambedue sono rappresentazioni valide:
+[97, 98, 99, 120, (258, 1), (41, 258), 97, 98, 99, 100, 97, 98, 99, 97, 98, 99]
+[97, 98, 99, 120, (258, 1), (41, 258), 97, 98, 99, 100, (3, 4), (3, 7)]
+Tuttavia sarebbe auspicabile la più compatta? e cioé:
+[97, 98, 99, 120, (258, 1), (41, 1), (3, 303), 100, (3, 4), (3, 7)]
+
+Input = b'abcxbcdexabcde'
+find_matches_hc3 trova il primo match di 3 su 'abc'
+[97, 98, 99, 120, 98, 99, 100, 101, 120, (3, 9), 100, 101]
+invece è preferibile trovare il match di 4 su 'bcde', in modo "lazy"
+[97, 98, 99, 120, 98, 99, 100, 101, 120, 97, (4, 6)] """
 
 def print_matches(matches, tab):
     offset=0
@@ -15,7 +81,6 @@ def print_matches(matches, tab):
         offset+=m[0]
     LOG.commit()
 
-import sqlite3, datetime
 # select count(len) from my where len=3 and dist=1 union select count(len) from zlib where len=3 and dist=1;
 # select sum(bytes) from (select len * count(len) as bytes from "a.zip" group by len);
 #~ SELECT 'my' AS source, COUNT(len) AS total
